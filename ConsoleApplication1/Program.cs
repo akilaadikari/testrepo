8a219e6654198db5ce4c1be900ff007b6929771c
akilaa <akila.adi@gmail.com>
2015-06-26 21:59:20 +0530

    first commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication1.OacIntegrationService;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AppointmentsClient client = new AppointmentsClient();
+            //List<Appointment> lst = client.GetAppointmentsForOLTIntegration(DateTime.Parse("2015-03-08 14:53:00.000"), DateTime.Parse("2015-03-09 14:53:00.000"), DateTime.Parse("2015-03-02 15:53:30.097"), "1haxXDu273677aOL");
+            //Console.ReadKey();
+
+            DateTime dt = new DateTime(DateTime.Now.Year, 12, 1);
+            Console.WriteLine(13 % 12);
+            Console.ReadKey();
+        }
+    }
+}
